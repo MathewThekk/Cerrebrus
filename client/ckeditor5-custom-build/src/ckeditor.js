b793cc01a7fe0fca584d49dfb89f05036f6c5821
Mathew Thekkadayil <71290474+MathewThekk@users.noreply.github.com>
2023-07-03 14:29:44 +0200

    fixed indent issue with ckeditor

--- client/ckeditor5-custom-build/src/ckeditor.js
+++ client/ckeditor5-custom-build/src/ckeditor.js
@@ -22,6 +22,7 @@ import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js"
 import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js"
 import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js"
 import Indent from "@ckeditor/ckeditor5-indent/src/indent.js"
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock"
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js"
 import Link from "@ckeditor/ckeditor5-link/src/link.js"
 import List from "@ckeditor/ckeditor5-list/src/list.js"
@@ -30,7 +31,6 @@ import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed.js"
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js"
 import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js"
 import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat.js"
-
 import Table from "@ckeditor/ckeditor5-table/src/table.js"
 import TableCaption from "@ckeditor/ckeditor5-table/src/tablecaption.js"
 import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties"
@@ -42,21 +42,39 @@ import Title from "@ckeditor/ckeditor5-heading/src/title.js"
 import { Style } from "@ckeditor/ckeditor5-style"
 import { GeneralHtmlSupport } from "@ckeditor/ckeditor5-html-support"
 import { Emoji, EmojiActivity, EmojiFlags, EmojiFood, EmojiNature, EmojiObjects, EmojiPeople, EmojiPlaces, EmojiSymbols } from "@phudak/ckeditor5-emoji/src"
+import { AutoImage } from "@ckeditor/ckeditor5-image"
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
+import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
+import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
+import SelectAll from '@ckeditor/ckeditor5-select-all/src/selectall.js';
+import { ShowBlocks } from '@ckeditor/ckeditor5-show-blocks';
+import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+
 
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
   Alignment,
+  AutoImage,
   Autoformat,
+  AutoLink,
   BlockQuote,
   Bold,
   CloudServices,
   Code,
   CodeBlock,
   Essentials,
+  FindAndReplace,
+  FontBackgroundColor,
   FontColor,
   FontFamily,
+  GeneralHtmlSupport,
   Heading,
   Image,
   ImageCaption,
@@ -66,15 +84,23 @@ Editor.builtinPlugins = [
   ImageToolbar,
   ImageUpload,
   Indent,
+  IndentBlock,
   Italic,
   Link,
   List,
   ListProperties,
   MediaEmbed,
+  MediaEmbedToolbar,
   Paragraph,
   PasteFromOffice,
   RemoveFormat,
-  
+  SelectAll,
+  ShowBlocks,
+  SourceEditing,
+  Strikethrough,
+  Style,
+  Subscript,
+  Superscript,
   Table,
   TableCaption,
   TableCellProperties,
@@ -83,8 +109,7 @@ Editor.builtinPlugins = [
   TableToolbar,
   TextTransformation,
   Title,
-  Style,
-  GeneralHtmlSupport,
+  Underline,
   Emoji,
   EmojiPeople,
   EmojiNature,
@@ -93,13 +118,48 @@ Editor.builtinPlugins = [
   EmojiActivity,
   EmojiObjects,
   EmojiSymbols,
-  EmojiFlags,
+  EmojiFlags
 ]
+
 
 // Editor configuration.
 Editor.defaultConfig = {
   toolbar: {
-    items: ["heading", "|", "bold", "italic", "link", "bulletedList", "numberedList", "|", "outdent", "indent", "alignment", "|", "codeBlock", "code", "imageUpload", "blockQuote", "insertTable", "mediaEmbed", "fontFamily", "fontColor", "imageInsert", "removeFormat", "emoji", "style"],
+    items: [
+			'heading',
+			'style',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'|',
+			'outdent',
+			'indent',
+			'alignment',
+			'fontFamily',
+			'fontColor',
+			'fontBackgroundColor',
+      "emoji",
+			'|',
+			'codeBlock',
+			'code',
+			'imageUpload',
+			'blockQuote',
+			'insertTable',
+			'mediaEmbed',
+			'removeFormat',
+			'showBlocks',
+			'sourceEditing',
+			'strikethrough',
+			'subscript',
+			'superscript',
+			'imageInsert',
+			'findAndReplace'
+		]
+
   },
   style: {
     definitions: [
@@ -160,6 +220,17 @@ Editor.defaultConfig = {
         styles: true,
       },
     ],
+    heading: {
+      options: [
+        { model: "paragraph", title: "Paragraph", class: "ck-heading_paragraph" },
+        { model: "heading1", view: "h1", title: "Heading 1", class: "ck-heading_heading1" },
+        { model: "heading2", view: "h2", title: "Heading 2", class: "ck-heading_heading2" },
+        { model: "heading3", view: "h3", title: "Heading 3", class: "ck-heading_heading3" },
+        { model: "heading4", view: "h4", title: "Heading 4", class: "ck-heading_heading4" },
+        { model: "heading5", view: "h5", title: "Heading 5", class: "ck-heading_heading5" },
+        { model: "heading6", view: "h6", title: "Heading 6", class: "ck-heading_heading6" },
+      ],
+    },
     disallow: [
       {
         attributes: [
@@ -172,8 +243,36 @@ Editor.defaultConfig = {
     ],
   },
   language: "en",
+  list: {
+    properties: {
+      styles: true,
+      startIndex: true,
+      reversed: true,
+    },
+  },
   image: {
-    toolbar: ["imageTextAlternative", "toggleImageCaption", "imageStyle:inline", "imageStyle:block", "imageStyle:side"],
+    styles: ["alignCenter", "alignLeft", "alignRight"],
+    resizeOptions: [
+      {
+        name: "resizeImage:original",
+        label: "Default image width",
+        value: null,
+      },
+      {
+        name: "resizeImage:50",
+        label: "50% page width",
+        value: "50",
+      },
+      {
+        name: "resizeImage:75",
+        label: "75% page width",
+        value: "75",
+      },
+    ],
+    toolbar: ["imageTextAlternative", "toggleImageCaption", "|", "imageStyle:inline", "imageStyle:wrapText", "imageStyle:breakText", "imageStyle:side", "|", "resizeImage"],
+    insert: {
+      integrations: ["insertImageViaUrl"],
+    },
   },
   table: {
     contentToolbar: ["tableColumn", "tableRow", "mergeTableCells", "tableCellProperties", "tableProperties"],
@@ -181,7 +280,6 @@ Editor.defaultConfig = {
 }
 
 export default Editor
-
 
 // SpecialCharacters,
 //   SpecialCharactersEssentials,
